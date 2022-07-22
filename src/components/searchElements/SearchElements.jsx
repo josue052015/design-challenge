a0e6578ec2a1fb4fa79f960e20a772e77b834dfa
manueldev94 <76499684+josue052015@users.noreply.github.com>
2022-07-22 01:31:26 -0400

    search fixed

--- src/components/searchElements/SearchElements.jsx
+++ src/components/searchElements/SearchElements.jsx
@@ -4,10 +4,6 @@ import styles from './styles.css'
 const SearchElements = () => {
     return (
         <>
-            {/*   <div className='dropdown'>
-        <p>klkkkk</p>
-        </div> */}
-
             <div className='search-elements-content'>
                 <div className='search-element'>
                     <span className='dropdown-text'>Select a game <i className="fa dropdown-icon">&#xf0d7;</i></span>
